de42687b72f9fd56321bc5b5ef8cf76c7334d107
Mateus Leonardo <m4teusmatos@gmail.com>
2021-11-27 16:08:06 -0300

    :art: changing style the items the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,25 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import style from '../../styles/home.module.css';
+import Car from '../../assets/images/car.png';
+import Image from 'next/image';
 
 const Header = () =>{
     return(
         <div className={style.header}>
-            <div className={style.childHeader}>
+            <div className={style.center}>
                 <a href="#" className={style.title}>AudioClub</a>
+                <div className={style.centerItems}>
+                    <a href="#" className={style.items}>Home</a>
+                    <a href="#" className={style.items}>Headphones</a>
+                    <a href="#" className={style.items}>Speakers</a>
+                    <a href="#" className={style.items}>Earphones</a>
+                    <Image
+                        src={Car}
+                        width={35}
+                        height={35}
+                    />
+                </div>
             </div>
         </div>
     );
